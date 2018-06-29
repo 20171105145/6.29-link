ede56d0aca7e6be420d1c7769028a896d9146983
20171105145 <20171105145du@admindeiMac-62.local>
2018-06-29 19:38:25 +0800

    Initial Commit

---
+++ 6.29 link/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  6.29 link
+//
+//  Created by 杜炎峰 on 2018/6/29.
+//  Copyright © 2018年 杜炎峰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
